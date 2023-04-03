ea7c413ce26eda7037750c6e34223d98ef918317
bily5354 <2587547190@qq.com>
2023-04-03 21:04:38 +0800

    update day14

--- day14/02.py
+++ day14/02.py
@@ -20,22 +20,24 @@ fileLock = Lock()  # 定义文件锁
 txtpath = "day14/taskList.txt"
 
 
-def downloadwz(theadidx, txtpath,task):
+# todo theadidx 线程序号  task 下载任务的网址 getText 下载后的字典
+def downloadwz(theadidx, task,getText):
+    
+    res = requests.get(task)
+    getText[task] = res.text
+
+
     fileLock.acquire()  # 获取锁
-
     with open(txtpath, 'a+', encoding="utf8") as f:
-        # print(task)
-        res = requests.get(task)
-        # print(res.text)
-        f.write(res.text)
+        f.write(getText[task])
         f.write("\n")
-
     fileLock.release()
 
 
 theadlist = []
+getText = {}  # 键是下载的链接 值是返回的数据
 for idx in range(len(taskList)):  # 循环次数是 tasklist的长度
-    thread = Thread(target=downloadwz, args=(idx, txtpath, taskList[idx]))
+    thread = Thread(target=downloadwz, args=(idx, taskList[idx], getText))
     thread.start()
     theadlist.append(thread)  # 加入线程列表 后面按顺序停止
 
