78bbc7f917bd51c85ca6f2a67ccad78d1322e861
bily5354 <2587547190@qq.com>
2023-04-13 15:54:03 +0800

    update sday2

--- 03selenium/day2/02.py
+++ 03selenium/day2/02.py
@@ -1,15 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-# 创建 webdriver 对象
 wd = webdriver.Chrome()
 
-# 调用 webdriver 对象 打开指定网址
 wd.get("http://127.0.0.1/mgr/sign.html")
 
 element = wd.find_element(By.ID,'username')
+element.send_keys('byh')
 
-element.send_keys('byhy')
+element = wd.find_element(By.ID,'password')
+element.send_keys('88888888')
 
 wd.find_element(By.CSS_SELECTOR,'button[type=submit]').click()
 
