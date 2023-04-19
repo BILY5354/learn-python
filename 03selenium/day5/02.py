32ca633fe30a7f99d23a51e5df701d431de3c0cf
bily5354 <2587547190@qq.com>
2023-04-19 13:39:44 +0800

    update sday5

--- 03selenium/day5/02.py
+++ 03selenium/day5/02.py
@@ -8,7 +8,12 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+# 记录所有股票与代码
 stock_dict = {}
+# 记录一个股票的信息
+info_dict = {}
+# 记录全有股票的
+stock_info_dict = {}
 
 wd = webdriver.Chrome()
 wd.implicitly_wait(5)
@@ -19,7 +24,7 @@ sleep(2)
 
 eles = wd.find_elements(By.CSS_SELECTOR, "tbody > tr")
 # 循环10次
-i = 0
+totalNu = 0
 # * 读取的是10页
 # ele 是每一行
 for ele in eles:
@@ -28,20 +33,59 @@ for ele in eles:
         By.CSS_SELECTOR, 'td:nth-child(2),td:nth-child(3)')
     # k是股票名称 v是股票代码
     stock_dict[stock[1].text] = stock[0].text
-    i = i+1
+    totalNu = totalNu+1
 
-    #* 执行点击代码
+    # * 执行点击代码
+    oldWin = wd.current_window_handle
+    # 点击数据按钮
+    ele.find_element(
+        By.CSS_SELECTOR, 'td:nth-child(4) > a:nth-child(3)').click()
+    sleep(2)
+    # 切换新窗口
+    stockName = stock[1].text
+    for handle in wd.window_handles:
+        wd.switch_to.window(handle)
+        # 得到标题栏字符串 判断是否是需操作的窗口
+        if stockName in wd.title:
+            # print(wd.title)
+            # 执行点击公司概况
+            wd.find_element(By.CSS_SELECTOR, 'li[data-href="gsgk"]').click()
+            sleep(2)
+
+            # 简介中所有标题 8个
+            titles = wd.find_elements(By.CSS_SELECTOR, '#gsgk tbody .bg')
+            contents = wd.find_elements(
+                By.CSS_SELECTOR, '#gsgk #gsjj_ggmc,#gsjj_frdb,#gsjj_zcdz,#gsjj_zczb,#gsjj_clrq,#gsjj_ssrq,#gsjj_zyfw,#gsjj_gsjj')
+
+            # 信息有8个
+            for i in range(0, 8):
+                info_dict[titles[i].text] = contents[i].text
+
+            stock_info_dict[stockName] = info_dict
+
+            break
+
+    # 回到旧窗口
+    wd.switch_to.window(oldWin)
+    sleep(1)
 
     # 10 次跳出循环
-    if i == 10:
+    if totalNu == 2:
         break
 
-print(stock_dict)
-    
+for k,v in stock_dict.items():
+    print(f'{k}\t{v}')
+
+for k,v in stock_info_dict.items():
+    print(f'{k}\t{v}')
+
 wd.quit()
 
 
-# # 引用 csv 模块
-# csv_file = open('03selenium/day5/test.csv', 'w', newline='', encoding='gbk')
-# # 调用open函数打开csv文件
-# writer = csv.writer(csv_file)
+
+# 引用 csv 模块
+with open('03selenium/day5/test.csv','w',newline='',encoding='gbk') as f:
+    writer = csv.writer(f)
+    writer.writerow(str(stock_dict))
+    writer.writerow(str(info_dict))
+    writer.writerow(str(stock_info_dict))
