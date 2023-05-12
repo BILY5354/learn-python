c18fe146fff5f3c39b0b5ac2057eebc22d7d1ed3
bily5354 <2587547190@qq.com>
2023-05-12 17:15:59 +0800

    update

---
+++ my fun/01rmq_hello/send.py
@@ -0,0 +1,20 @@
+import pika
+
+# connection = pika.BlockingConnection(
+#     pika.ConnectionParameters(host='192.168.0.120',
+#                               port=5672,
+#                               '/',
+#                               credentials=credentials))
+credentials  = pika.PlainCredentials('admin','123456')
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters('192.168.0.120',
+                              5672,
+                              '/',
+                              credentials))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='', routing_key='hello', body='New Hello World!')
+print(" [x] Sent 'Hello World!'")
+connection.close()
